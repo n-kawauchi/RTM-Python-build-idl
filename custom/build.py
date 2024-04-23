23bc656dbf0b835116fe633882049001f8ad6b21
n-kawauchi <n.kawauchi@aist.go.jp>
2024-04-23 11:21:42 +0900

    Removed omniidl '-Wbpackage' option.

--- custom/build.py
+++ custom/build.py
@@ -47,7 +47,7 @@ class BuildIDL(Command):
             self.omniidl = 'omniidl'
         if not self.stubs_dir:
             self.set_undefined_options('build', ('build_base', 'stubs_dir'))
-            self.stubs_dir = os.path.join(self.stubs_dir, 'stubs')
+            self.stubs_dir = os.path.join(self.stubs_dir, 'stubs', 'OpenRTM_aist', 'RTM_IDL')
         if not self.idl_dir:
             self.set_undefined_options('build', ('build_base', 'idl_dir'))
             self.idl_dir = os.path.join(self.idl_dir, 'OpenRTM_aist/RTM_IDL')
@@ -55,11 +55,12 @@ class BuildIDL(Command):
         self.examples_dir = os.path.join(os.getcwd(), 'OpenRTM_aist/examples')
         self.set_undefined_options('build', ('build_lib', 'build_lib'))
 
-    def compile_one_idl(self, idl_f, pkg_param, outdir):
+    #def compile_one_idl(self, idl_f, pkg_param, outdir):
+    def compile_one_idl(self, idl_f, outdir):
         outdir_param = '-C' + outdir
         idl_path_param = '-I' + 'OpenRTM_aist/RTM_IDL'
         p = subprocess.Popen([self.omniidl, '-bpython', idl_path_param,
-                              outdir_param, pkg_param, idl_f],
+                              outdir_param, idl_f],
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = p.communicate()
         if p.returncode != 0:
@@ -70,10 +71,10 @@ class BuildIDL(Command):
     def compile_idl(self):
         log.info('***Generating Python stubs from IDL files')
         self.mkpath(self.stubs_dir)
-        pkg_param = '-Wbpackage=OpenRTM_aist.RTM_IDL'
+        #pkg_param = '-Wbpackage=OpenRTM_aist.RTM_IDL'
         idl_files = [os.path.join(self.idl_src_dir, f) for f in baseidl_files]
         for f in idl_files:
-            self.compile_one_idl(f, pkg_param, self.stubs_dir)
+            self.compile_one_idl(f, self.stubs_dir)
 
     def move_stubs(self):
         stub_dest = os.path.join(self.build_lib, 'OpenRTM_aist', 'RTM_IDL')
